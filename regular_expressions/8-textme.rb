77f282830dcb0a5e2ceabab3b7bface95358bd8e
pmiyienda-dotcom <p.miyienda@alustudent.com>
2026-05-10 13:46:17 -0700

    regex

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,8 +1,3 @@
 #!/usr/bin/env ruby
-
-# Extract sender, receiver and flags using regex
-sender = ARGV[0].scan(/\[from:(.*?)\]/).flatten.first || ""
-receiver = ARGV[0].scan(/\[to:(.*?)\]/).flatten.first || ""
-flags = ARGV[0].scan(/\[flags:(.*?)\]/).flatten.first || ""
-
-puts "#{sender},#{receiver},#{flags}"
+# 8. Textme
+puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
