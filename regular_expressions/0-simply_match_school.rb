aed6a33fc4040e491f477a10972b7dcb4f8257be
pmiyienda-dotcom <p.miyienda@alustudent.com>
2026-05-10 14:01:49 -0700

    regex

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# Neha loves School, that is why she is a School mentor :) 
+# Regular expression must matches school, this script is written by Latjor
 puts ARGV[0].scan(/School/).join
